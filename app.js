d1eb9e4d367594efe0ecd2975cdea1076ff78b83
Subritt <subrittburlakoti@gmail.com>
2020-07-23 22:05:52 +0545

    addBookToList function created

--- app.js
+++ app.js
@@ -28,11 +28,28 @@ class UI {
 
         books.forEach((book) => UI.addBookToList(book));
     }
+
+    // Adds book to Table
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm 
+            delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
 }
 
 // Store Class: Handles Storage
 
 // Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
 
